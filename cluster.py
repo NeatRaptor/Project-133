3c6bfad7f1b6b5e74b17f4653d92e34b799830db
Roshan K <74712665+NeatRaptor@users.noreply.github.com>
2022-05-09 10:58:44 +0530

    Add files via upload

---
+++ cluster.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv('data.csv')
+
+X = df.iloc[:,[3,4]].values
+
+wcss = []
+
+for i in range(1, 11):
+    try:
+        kmeans = KMeans(n_clusters=i, init='k-means++', random_state = 42)
+        kmeans.fit(X)
+        wcss.append(kmeans.inertia_)
+    except:pass
+plt.figure(figsize=(10,5))
+sns.lineplot(range(1, 11), wcss, marker='o', color='red')
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
